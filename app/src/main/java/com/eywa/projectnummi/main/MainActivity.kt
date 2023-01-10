4c744b65657f82e5b4082cfa6b3b04ee5ea83d99
Ewa <ella.caldeirahankey@ntlworld.com>
2023-01-10 20:46:05 +0000

    Added a way to save state when navigating away from a destination. MainNavRoute routes now derive from their name

--- app/src/main/java/com/eywa/projectnummi/main/MainActivity.kt
+++ app/src/main/java/com/eywa/projectnummi/main/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.navigation.NavArgument
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.eywa.projectnummi.ui.theme.NummiTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,10 +21,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val navRoutes = MainNavRoute.values()
+        val duplicateRoutes = navRoutes
+                .groupBy { it.name.lowercase() }
+                .filter { (_, v) -> v.size > 1 }
+                .keys
+        if (duplicateRoutes.isNotEmpty()) {
+            throw IllegalStateException("Duplicate navRoutes found: " + duplicateRoutes.joinToString(","))
+        }
+
         setContent {
             NummiTheme {
                 val navController = rememberNavController()
-                val navRoutes = MainNavRoute.values()
+                val currentEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = currentEntry?.destination?.route
 
                 window.navigationBarColor = NummiTheme.colors.androidNavButtons.toArgb()
                 window.statusBarColor = NummiTheme.colors.statusBar.toArgb()
@@ -29,8 +44,24 @@ class MainActivity : ComponentActivity() {
                         backgroundColor = NummiTheme.colors.appBackground.main,
                         bottomBar = {
                             NummiBottomNav(
-                                    navController = navController,
-                                    onClick = { navController.navigate(it.routeBase) },
+                                    currentRoute = currentRoute,
+                                    onClick = {
+                                        navController.navigate(it.routeBase) {
+                                            fun saveState(route: String?, args: Map<String, NavArgument>?) =
+                                                    it.routeBase != currentRoute
+                                                            && route
+                                                            ?.toMainNavRoute()
+                                                            ?.saveStateOnNavigate(args)
+                                                            ?: false
+
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState =
+                                                        saveState(currentRoute, currentEntry?.destination?.arguments)
+                                            }
+                                            restoreState = saveState(it.routeBase, null)
+                                            launchSingleTop = true
+                                        }
+                                    },
                             )
                         }
                 ) { padding ->
--- app/src/main/java/com/eywa/projectnummi/main/MainNavRoute.kt
+++ app/src/main/java/com/eywa/projectnummi/main/MainNavRoute.kt
@@ -1,6 +1,7 @@
 package com.eywa.projectnummi.main
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavArgument
 import androidx.navigation.NavHostController
 import com.eywa.projectnummi.features.addTransactions.AddTransactionsScreen
 import com.eywa.projectnummi.features.manageAccounts.ManageAccountsScreen
@@ -8,35 +9,42 @@ import com.eywa.projectnummi.features.manageCategories.ManageCategoriesScreen
 import com.eywa.projectnummi.features.managePeople.ManagePeopleScreen
 import com.eywa.projectnummi.features.viewTransactions.ViewTransactionsScreen
 
+fun String.toMainNavRoute() =
+        try {
+            MainNavRoute.valueOf(uppercase())
+        }
+        catch (e: IllegalArgumentException) {
+            null
+        }
+
 enum class MainNavRoute : NavRoute {
     ADD_TRANSACTIONS {
-        override val routeBase: String = "add_transactions"
-
         @Composable
         override fun Screen(navController: NavHostController) = AddTransactionsScreen()
+
+        override fun saveStateOnNavigate(arguments: Map<String, NavArgument>?): Boolean {
+            return true
+        }
     },
     VIEW_TRANSACTIONS {
-        override val routeBase: String = "view_transactions"
-
         @Composable
         override fun Screen(navController: NavHostController) = ViewTransactionsScreen()
     },
     MANAGE_CATEGORIES {
-        override val routeBase: String = "manage_categories"
-
         @Composable
         override fun Screen(navController: NavHostController) = ManageCategoriesScreen()
     },
     MANAGE_PEOPLE {
-        override val routeBase: String = "manage_people"
-
         @Composable
         override fun Screen(navController: NavHostController) = ManagePeopleScreen()
     },
     MANAGE_ACCOUNTS {
-        override val routeBase: String = "manage_accounts"
-
         @Composable
         override fun Screen(navController: NavHostController) = ManageAccountsScreen()
     },
+    ;
+
+    override val routeBase = "mainnavroute_" + name.lowercase()
+
+    open fun saveStateOnNavigate(arguments: Map<String, NavArgument>?): Boolean = false
 }
